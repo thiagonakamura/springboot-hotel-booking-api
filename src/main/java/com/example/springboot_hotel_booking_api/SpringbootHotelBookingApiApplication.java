6229c8f1c694d47287679a17908fce8dc45f2d7b
Thiago Nakamura <thiagokenzo@outlook.com>
2025-03-31 19:24:34 -0300

    Project created

---
+++ src/main/java/com/example/springboot_hotel_booking_api/SpringbootHotelBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_hotel_booking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHotelBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHotelBookingApiApplication.class, args);
+	}
+
+}
